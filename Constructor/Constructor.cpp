dafeb2b82cd1b903f83978b753b59f2d5e01fda3
ASUS <ASUS@AZIZDWIMUKTI>
2024-05-13 15:54:43 +0700

    int main

--- Constructor/Constructor.cpp
+++ Constructor/Constructor.cpp
@@ -4,5 +4,55 @@ using namespace std;
 
 class Mahasiswa
 {
+public:
+	int nim;
+	string nama;
 
+	Mahasiswa()
+	{
+		nim = 0;
+		nama = "";
+
+	};
+	Mahasiswa(int iNim)
+	{
+		nim = iNim;
+
+
+	};
+	
+	Mahasiswa(string);
+	Mahasiswa(int iNim, string iNama);
+	void cetak()
+	{
+		cout << "NIM = " << nim << endl; 
+		cout << "Nama = " << nama << endl;
+		cout << endl;
+
+	}
 };
+
+Mahasiswa::Mahasiswa(string iNama) {
+	nama = iNama;
+}
+
+Mahasiswa::Mahasiswa(int iNim, string iNama)
+{
+	nim = iNim;
+	nama = iNama;
+}
+
+int main()
+{
+	Mahasiswa mhs1;
+	Mahasiswa mhs2("087");
+	Mahasiswa mhs3("Azis");
+	Mahasiswa mhs4(100,"Azis");
+
+	mhs1.cetak();
+	mhs2.cetak();
+	mhs3.cetak();
+	mhs4.cetak();
+
+	return 0;
+}
