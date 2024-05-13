d587f7dffaaf2117f031ee505c18ee080061f3bc
ASUS <ASUS@AZIZDWIMUKTI>
2024-05-13 15:08:11 +0700

    membuat akses modifier class mahasiswa

--- MemberStaticFunction/MemberStaticFunction.cpp
+++ MemberStaticFunction/MemberStaticFunction.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
-using namepsace std;
+#include <string>
+
+using namespace std;
 
 class Mahasiswa
 {
+private:
+	static int nim;
 
+public:
+	int id;
+	string nama;
+
+	void setID();
+	void printAll();
+
+	static void setNim(int pNim) { nim = pNim; }
+	static int getNim() { return nim; }
+
+	Mahasiswa(string pnama) :nama(pnama)
+	{
+		setID();
+	}
 };
